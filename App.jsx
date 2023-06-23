5c317bb2427418fa0301adf765a375ab97a6e1fb
BSTAX <136502318+BSTAX@users.noreply.github.com>
2023-06-23 22:57:19 +0530

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
+import Todolist from "./Todolist";
 
 const App = () => {
   const [inputlist, setinputlist] = useState();
-  const [items,setitems]=useState([]);
+  const [items, setitems] = useState([]);
 
   const itemEvent = (event) => {
     setinputlist(event.target.value);
   };
 
   const listofitems = () => {
+    setitems((olditems) => {
+      return [...olditems, inputlist];
+    });
+    // This line will clear whatever written in the input field after adding to the list.
+    setinputlist("");
+  };
 
+  const deleteitems = (id) => {
+    console.log("deleted");
+
+    setitems((olditems)=>{
+      return olditems.filter((arrelem,index)=>{
+          return index !== id;
+      })
+    })
   };
 
   return (
@@ -19,13 +34,25 @@ const App = () => {
           <br />
           <h1>To-Do List</h1>
           <br />
-          <input type="text" placeholder="Add a item" onChange={itemEvent} />
+          <input
+            type="text"
+            placeholder="Add a item"
+            onChange={itemEvent}
+            value={inputlist}
+          />
           <button onClick={listofitems}> + </button>
           <ol>
             {/* <li>{inputlist}</li> */}
- 
-            {items.map((itemval)=>{
-              return <li>{inputlist}</li>
+
+            {items.map((itemval, index) => {
+              return (
+                <Todolist
+                  key={index}
+                  id={index}
+                  text={itemval}
+                  onSelect={deleteitems}
+                /> 
+              );
             })}
           </ol>
         </div>
